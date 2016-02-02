6653663e444950b46d5230d97721a299ffef5e7b
Matt <mb@issuu.com>
2016-02-01 22:14:43 -0800

    Better "Nothing playing" state. Shows video info.
    #2 #4 #10

--- src/handler-main.js
+++ src/handler-main.js
@@ -9,7 +9,8 @@ var mainUiComponents = require('./screen-main');
 
 var volume = 0;
 var playerid = 0;
-var isPlaying = false;
+var playertype = 'audio';
+var playState = 0;
 
 var titleUi;
 var descriptionUi;
@@ -45,24 +46,35 @@ module.exports.updatePlayerState = function() {
     api.send('Player.GetActivePlayers', [], function(data) {
         if (data.result.length > 0) {
             playerid = data.result[0].playerid;
+            playertype = data.result[0].type;
 
             api.send('Player.GetProperties', [playerid, ['percentage', 'speed']], function(data) {
-                isPlaying = data.result.speed > 0;
-
                 var actionDef = mainUiComponents.actionDef;
-                if (isPlaying) {
+                if (data.result.speed > 0) {
+                    playState = 2;
                     actionDef.select = 'images/pause.png';
                     mainScreen.action(actionDef);
                 } else {
+                    playState = 1;
                     actionDef.select = 'images/play.png';
                     mainScreen.action(actionDef);
                 }
             });
 
-            api.send('Player.GetItem', {"properties": ["title", "album", "artist", "duration"], "playerid": playerid}, function(data) {
+            api.send('Player.GetItem', {'properties': ['title', 'album', 'artist', 'duration', 'runtime', 'showtitle'], 'playerid': playerid}, function(data) {
                 var playingItem = data.result.item;
-                updateText(playingItem.title, playingItem.artist[0].toUpperCase() + '\n' + playingItem.album);
+                if (playertype === 'audio') {
+                    updateText(playingItem.title, playingItem.artist[0].toUpperCase() + '\n' + playingItem.album);
+                } else if (playertype === 'video') {
+                    updateText(playingItem.title, playingItem.showtitle || '');
+                }
             });
+        } else {
+            playState = 0;
+            updateText('Nothing playing', 'Press play to ' + (Settings.option('playActionWhenStopped') === 'playLast' ? 'start the last active playlist' : 'start the party'));
+            var actionDef = mainUiComponents.actionDef;
+            actionDef.select = 'images/play.png';
+            mainScreen.action(actionDef);
         }
     });
 };
@@ -79,9 +91,23 @@ module.exports.init = function(m) {
     module.exports.updatePlayerState();
 
     mainScreen.on('click', 'select', function(e) {
-        api.send('Player.PlayPause', [playerid, 'toggle'], module.exports.updatePlayerState);
-
-        mixpanel.track('Button pressed, PlayPause');
+        if (playState > 0) {
+            api.send('Player.PlayPause', [playerid, 'toggle'], module.exports.updatePlayerState);
+        } else {
+            switch (Settings.option('playActionWhenStopped')) {
+                case 'playLast':
+                    api.send('Input.ExecuteAction', {'action': 'play'}, module.exports.updatePlayerState);  
+                    break;
+                case 'partymode':
+                    /*falls through*/
+                default:
+                    api.send('Player.SetPartymode', [playerid, 'toggle'], module.exports.updatePlayerState);
+                    break;
+            }
+        }
+        mixpanel.track('Button pressed, PlayPause', {
+            playState: playState
+        });
     });
 
     mainScreen.on('longClick', 'up', function(e) {
