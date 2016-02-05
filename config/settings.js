6d812534303a23e814d0668f71412e50184a0b4b
mattmattmatt <mb@issuu.com>
2016-02-04 21:51:55 -0800

    #19 Release new host auth settings.

--- config/settings.js
+++ config/settings.js
@@ -10,11 +10,12 @@ function getStateFromUi() {
         if (input.value.trim()) {
             settings.hosts.push({
                 address: input.value.trim(),
-                name: $('.js-host-name').get(index).value.trim()
+                name: $('.js-host-name').get(index).value.trim(),
+                username: $('.js-host-username').get(index).value.trim(),
+                password: $('.js-host-password').get(index).value.trim()
             })
         }
     });
-    settings.ip = settings.hosts[0] && settings.hosts[0].address || undefined;
     return settings;
 }
 
@@ -33,13 +34,15 @@ function setUiFromState(config) {
     if (!config.hosts || !config.hosts.length) {
         config.hosts = [{
             name: 'Kodi',
-            address: config.ip || ''
+            address: config.ip || '',
+            username: '',
+            password: ''
         }];
     }
 
     $('#host-container').empty();
     _.each(config.hosts, function(host, index) {
-        $('#host-container').append('<fieldset class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label"><input class="mdl-textfield__input js-host-address" type="text" '+ (index === 0 ? 'required="required"' : '') + ' autocapitalize="off" autocorrect="off" id="addr-'+index+'" value="'+host.address+'" ><label class="mdl-textfield__label" for="addr-'+index+'">Kodi IP Address</label></fieldset><fieldset class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label"><input class="mdl-textfield__input js-host-name" type="text" id="name-'+index+'"  value="'+host.name+'"><label class="mdl-textfield__label" for="name-'+index+'">Host Name</label></fieldset>');
+        $('#host-container').append('<fieldset class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label"><input class="mdl-textfield__input js-host-address" type="text" '+ (index === 0 ? 'required="required" ' : ' ') + ' autocapitalize="off" autocorrect="off" id="addr-'+index+'" value="'+host.address+'"><label class="mdl-textfield__label" for="addr-'+index+'">Kodi IP Address and Port (e.g. 192.168.1.100:8080)</label></fieldset><fieldset class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label"><input class="mdl-textfield__input js-host-name" type="text" id="name-'+index+'" value="'+host.name+'"><label class="mdl-textfield__label" for="name-'+index+'">Host Name</label></fieldset><fieldset class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label"><input class="mdl-textfield__input js-host-username" type="text" id="username-'+index+'" value="'+host.username+'" autocapitalize="off" ><label class="mdl-textfield__label" for="username-'+index+'">Username (optional)</label></fieldset><fieldset class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label"><input class="mdl-textfield__input js-host-password" type="text" id="password-'+index+'" value="'+host.password+'" autocapitalize="off" autocorrect="off" autocomplete="off" ><label class="mdl-textfield__label" for="password-'+index+'">Password (optional)</label></fieldset><hr />');
     });
     componentHandler.upgradeElements($('#host-container').get(0));
 }
