142a54bcb812ba6159702aa7bc04738722a9222e
Matt <mb@issuu.com>
2016-02-19 00:23:22 -0800

    #32 New version's features teased in app

--- src/screen-update-notification.js
+++ src/screen-update-notification.js
@@ -6,10 +6,28 @@ var UI = require('ui');
 
 var notificationScreen;
 
-function showUpdateNotification(newestVersion, runningVersion) {
-    var bodyString = 'You are running Opus version ' + 
-        runningVersion + 
-        '. Visit the Pebble App Store to update to version ' +
+function getMessage(data, runningVersion) {
+    var message = '';
+    var changelogs = data.changelog;
+    var foundNewVersions = false;
+    var applicableChangelogs = changelogs.filter(function(entry) {
+        if (entry.version === runningVersion) {
+            foundNewVersions = true;
+        }
+        return !foundNewVersions;
+    });
+    applicableChangelogs.forEach(function(item) {
+        if (item.release_notes) {
+            message += item.release_notes + '\n';            
+        }
+    });
+    return message;
+}
+
+function showUpdateNotification(newestVersion, runningVersion, message) {
+    var bodyString = 'New since your version ' + runningVersion + ':\n';
+    bodyString += message || 'General bugfixes and stability improvements.\n';
+    bodyString += '\nVisit the Pebble App Store to update to version ' +
         newestVersion + 
         ' for free. 👍';
 
@@ -37,21 +55,24 @@ module.exports.getScreen = function() {
 module.exports.init = function() {
     ajax(
         {
-            url: 'https://raw.githubusercontent.com/mattmattmatt/opus-pebble/master/appinfo.json',
+            url: 'https://api2.getpebble.com/v2/apps/id/56ac4a755319ec979400000d',
             method: 'get'
         },
-        function(data, status, request) {
+        function(data) {
             try {
-                var newestVersion = JSON.parse(data).versionLabel;
+                data = JSON.parse(data).data[0];
+                var newestVersion = data.latest_release.version;
                 var runningVersion = require('../appinfo.json').versionLabel;
-                console.log(newestVersion + ' ' + runningVersion);
+                console.log(newestVersion + ' is available; you are running ' + runningVersion);
 
                 if (newestVersion !== runningVersion) {
-                    showUpdateNotification(newestVersion, runningVersion);
+                    showUpdateNotification(newestVersion, runningVersion, getMessage(data, runningVersion));
                 }
-            } catch(e) {}
+            } catch(e) {
+                console.log('JSON error: ' + JSON.stringify(e));
+            }
         },
-        function(error, status, request) {
+        function(error) {
             console.log('ajax error: ' + JSON.stringify(error));
         }
     );
